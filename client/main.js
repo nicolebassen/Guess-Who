51da1b7df921a3fffcfc0a3017ea41995d766f69
Nicole Bassen <nicolerbassen@gmail.com>
2016-04-29 03:30:52 -0700

    Added chat panel, mainbox template helpers, padding in panels; code cleanup

--- client/main.js
+++ client/main.js
@@ -65,6 +65,41 @@ var tiles = [
 
 Session.set('tile', tiles);
 
+// store all tiles in a variable
+var allTiles = Session.get('tile');
+
+// store the three rows of tiles separately
+var firstRow = [];
+var secondRow = [];
+var thirdRow = [];
+
+for (var i = 0; i < 5; i++) {
+	firstRow[i] = allTiles[i];
+	secondRow[i] = allTiles[i + 5];
+	thirdRow[i] = allTiles[i + 10];
+}
+console.log(firstRow);
+console.log(secondRow);
+console.log(thirdRow);
+
+Template.mainbox.helpers({
+	firstRow: function() {
+		return firstRow;
+	},
+	secondRow: function() {
+		return secondRow;
+	},
+	thirdRow: function() {
+		return thirdRow;
+	}
+});
+
+Template.mainbox.events({
+    'click': function(event, instance) {
+        // do something
+    }
+});
+
 
 
 
