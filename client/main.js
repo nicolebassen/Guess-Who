754f1f822d035642b12735ef6787a36a60348c36
Nicole Bassen <nicolerbassen@gmail.com>
2016-05-06 02:42:01 -0700

    Created counter, added event handler for clicking tiles, computer randomly selects a tile

--- client/main.js
+++ client/main.js
@@ -1,116 +1,141 @@
-
+// Nicole Bassen and Kimberly Praxel
+// Guess Who? Meteor website
 
 var tiles = [
 	{
 		name: "Kim",
 		frontImage: "kim.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Nicole",
 		frontImage: "nicole.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Chris",
 		frontImage: "christopher.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Richard",
 		frontImage: "richard.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Bob",
 		frontImage: "bob.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Kim",
 		frontImage: "kim.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Nicole",
 		frontImage: "nicole.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Chris",
 		frontImage: "christopher.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Richard",
 		frontImage: "richard.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Bob",
 		frontImage: "bob.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Kim",
 		frontImage: "kim.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Nicole",
 		frontImage: "nicole.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Chris",
 		frontImage: "christopher.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Richard",
 		frontImage: "richard.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	},
 	{
 		name: "Bob",
 		frontImage: "bob.jpg",
 		backImage: "tileback.png",
-		opponentBackImage: "redtile.png"
+		opponentBackImage: "redtile.png",
+		clickCounter: 0
 	}
 ];
 
 Session.set('tile', tiles);
 
+// keeps track of how many tiles are flipped over
+var tileCounter = 0;
+
 // store all tiles in a variable
 var allTiles = Session.get('tile');
+
+// tile that the player selects
+var myTile = "tileback.png";
+
+// computer opponent randomly selects a tile
+var random = Math.floor(Math.random() * 15);
+var opponentTile = allTiles[random];
+console.log(opponentTile.name);
 
 // store the three rows of tiles separately
 var firstRow = [];
 var secondRow = [];
 var thirdRow = [];
 
+// fill in the three rows of tiles
 for (var i = 0; i < 5; i++) {
 	firstRow[i] = allTiles[i];
 	secondRow[i] = allTiles[i + 5];
 	thirdRow[i] = allTiles[i + 10];
 }
-console.log(firstRow);
-console.log(secondRow);
-console.log(thirdRow);
 
 Template.mainbox.helpers({
 	firstRow: function() {
@@ -121,6 +146,9 @@ Template.mainbox.helpers({
 	},
 	thirdRow: function() {
 		return thirdRow;
+	},
+	myTile: function() {
+		return myTile;
 	}
 });
 
@@ -137,8 +165,20 @@ Template.opponentBoard.helpers({
 });
 
 Template.mainbox.events({
-    'click': function(event, instance) {
-        // do something
+    'click .playerTile': function(event, instance) {
+        if (tileCounter == 0) {
+        	// myTile = ??
+        }
+        // if (this.clickCounter??? % 2 = 0) {
+        //		this.clickCounter++		"flip tile"
+        // } else {
+        // 		this.clickCounter--		"unflip tile"
+        // }
+        tileCounter++;
+        if (tileCounter == 14) {
+        	console.log("Game over");
+        }
+        console.log(tileCounter);
     }
 });
 
