d4bd5375257377a85a1b085f90a9b471cbc1c9de
Nicole Bassen <nicolerbassen@gmail.com>
2016-05-12 15:27:09 -0700

    Added scrolling to chat, game messages are displayed, added online users list, fixed alignment

--- client/main.js
+++ client/main.js
@@ -140,8 +140,14 @@ Session.set('tile', tiles);
 // keeps track of how many tiles are flipped over
 var tileCounter = 0;
 
+// start game, results of game (win/loss)
+Session.set('gameMessage', "Choose a tile for your opponent to guess.");
+
 // store all tiles in a variable
 var allTiles = Session.get('tile');
+
+// onclick event to flip a tile
+var flipTile = Session.set('flipTile', "");
 
 // tile that the player selects
 var myTile = allTiles[15];
@@ -150,7 +156,7 @@ Session.set('myTile', myTile);
 // computer opponent randomly selects a tile
 var random = Math.floor(Math.random() * 15);
 var opponentTile = allTiles[random];
-console.log("The opponent's tile is: " + opponentTile.name);
+console.log("The opponent's tile is: " + opponentTile.name + ", ID: " + opponentTile.id);
 
 // store the three rows of tiles separately
 var firstRow = [];
@@ -176,6 +182,12 @@ Template.mainbox.helpers({
 	},
 	myTile: function() {
 		return Session.get('myTile');
+	},
+	flipTile: function() {
+		return Session.get('flipTile');
+	},
+	gameMessage: function() {
+		return Session.get('gameMessage');
 	}
 });
 
@@ -188,6 +200,9 @@ Template.opponentBoard.helpers({
 	},
 	thirdRow: function() {
 		return thirdRow;
+	},
+	opponentTile: function() {
+		return opponentTile;
 	}
 });
 
@@ -195,14 +210,21 @@ Template.mainbox.events({
     'click .playerTile': function(event) {
     	var id = event.currentTarget.id;
     	
+    	// set tile flipped property to true
+    	Session.set('flipTile', "this.classList.toggle('flipped')");
+    	
         if (tileCounter == 0) {
-        	event.preventDefault(); // prevent tile from flipping?
+        	// the tile the player is choosing
         	Session.set('myTile', allTiles[id]);
         	tileCounter++;
+        	
+        	Session.set('gameMessage', "Let's play!");
         	
         	// test
         	console.log("You chose: " + allTiles[id].id + " (" + allTiles[id].name + ")");
         } else {
+        	Session.set('gameMessage', "");
+        	
         	// if image side of tile was facing up
         	if (allTiles[id].flipped % 2 == 0) {
         		allTiles[id].flipped++;	// switch flipped to true
@@ -224,8 +246,20 @@ Template.mainbox.events({
         }
         console.log("Tile counter: " + tileCounter);  // how many tiles are flipped over
         
-        if (tileCounter >= 14) {
-        	console.log("Game over");
+        if (tileCounter >= 15) {
+        	for (var i = 0; i < allTiles.length; i++) {
+        		if (allTiles[i].flipped == 0) {
+        			if (allTiles[i].id == opponentTile.id) {
+        				Session.set('gameMessage', "That is your opponent's tile. You win!");
+        				//window.alert("Game over. You win!!");     				
+        				console.log("That is the opponent's tile. You win!!");
+        			} else {
+        				Session.set('gameMessage', "That is not your opponent's tile. You lose!");
+        				//window.alert("Game over. You lose!!");    
+        				console.log("That is not the opponent's tile. You lose!");
+        			}
+				}
+			}
         }  
     }
 });
