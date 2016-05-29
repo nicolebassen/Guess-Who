66b5254deda30b7d7f2f9c49f90bf1f542fdfd51
Nicole Bassen <nicolerbassen@gmail.com>
2016-05-28 23:09:14 -0700

    Minimal styling on instructions page

--- routing/routes.js
+++ routing/routes.js
@@ -1,6 +1,7 @@
 
 
 // define routes
+
 Router.route('/', function() {
     this.render('login');
 });
@@ -12,3 +13,16 @@ Router.route('/game', function() {
 Router.route('/instructions', function() {
     this.render('instructions');
 });
+/*
+Router.onBeforeAction(function() {
+    // make sure the user is logged in
+    if (!Meteor.user() && !Meteor.loggingIn()) {
+        // send to login page if not logged in or logging in
+        this.redirect('/login');
+    } else {
+        // must include
+        this.next(); // tells the router to continue with its business
+    }
+}, {
+    except: ['login']
+});*/
