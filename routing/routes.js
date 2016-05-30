2d11e86fda791df2f1f62aefe6d04c67fb33268e
Nicole Bassen <nicolerbassen@gmail.com>
2016-05-29 23:32:24 -0700

    Fixed redirect if not logged in, fixed typos in instructions

--- routing/routes.js
+++ routing/routes.js
@@ -26,11 +26,11 @@ Router.onBeforeAction(function() {
     // make sure the user is logged in
     if (!Meteor.user() && !Meteor.loggingIn()) {
         // send to login page if not logged in or logging in
-        Router.redirect('/login');
+        Router.go('/login');
     } else {
         // must include
         this.next(); // tells the router to continue with its business
     }
 }, {
-    except: ['login']
+    except: ['login', 'home']
 });
